4e03dde36ea94a8e5e61bfd529635a1d20939aa6
Mike Plekhanov <movb@fb.com>
2021-09-01 14:34:41 -0700

    Missing utils.py

---
+++ bela/utils/utils.py
@@ -0,0 +1,8 @@
+class DummyPathManager:
+    def get_local_path(self, path, *args, **kwargs):
+        return path
+
+    def open(self, path, *args, **kwargs):
+        return open(path, *args, **kwargs)
+
+PathManager = DummyPathManager()
