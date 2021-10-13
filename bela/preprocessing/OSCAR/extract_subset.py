0ff089ceb44ff1d4214d386831175d6a48c9a532
Nora Kassner <kassner@fb.com>
2021-10-13 09:13:00 +0200

    filter oscar

---
+++ bela/preprocessing/OSCAR/extract_subset.py
@@ -0,0 +1,63 @@
+import argparse
+import gzip
+import json
+import glob
+from newsplease import NewsPlease
+import tqdm
+import os
+
+
+def filter_data(base_path, url, name, date):
+    year = int(date.split('_')[0])
+    month = int(date.split('_')[1])
+    filenames = glob.glob(base_path + "/metadata/en_meta_part_*_.jsonl")
+    idx_dict = {}
+
+    for filename in tqdm.tqdm(filenames):
+        idx = filename.split("/")[-1].split("_")[-2]
+        idx_dict[idx] = []
+        with open(filename, 'rb') as f:
+            for line in f:
+                line = line.decode('utf-8')
+                line = json.loads(line)
+                if url in line['uri']:
+                    try:
+                        article = NewsPlease.from_url(line['uri'])
+                        line["timestamp"] = article.date_publish
+                        if line["timestamp"]:
+                            if line["timestamp"].year > year:
+                                idx_dict[idx].append(line)
+                            elif line["timestamp"].year >= year and line["timestamp"].month >= month:
+                                idx_dict[idx].append(line)
+                            else:
+                                idx_dict[idx].append(line)
+                    except:
+                        pass
+
+    with open(base_path + "/subset/" + name + ".json", "w") as f:
+        json.dump(idx_dict)
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--url",
+        type=str,
+        help="Base folder with Wikipedia data.",
+    )
+    parser.add_argument(
+        "--name",
+        type=str,
+        help="Base folder with Wikipedia data.",
+    )
+    parser.add_argument(
+        "--base_path",
+        type=str,
+    )
+    parser.add_argument(
+        "--date",
+        type=str,
+    )
+    args, _ = parser.parse_known_args()
+    filter_data(args.base_path, args.url, args.name, args.date)
