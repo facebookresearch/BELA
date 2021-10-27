0d0b0eadb38f1f3a6aec4f3702f6b0481d9f7acf
Nora Kassner <kassner@fb.com>
2021-10-27 10:00:17 +0200

    main embedding

---
+++ bela/embed_mentions.py
@@ -0,0 +1,35 @@
+# (c) Facebook, Inc. and its affiliates. Confidential and proprietary.
+
+import hydra
+from bela.conf.config import MainConfig
+import os.path
+from bela.datamodule.entity_encoder import embed
+
+from omegaconf import OmegaConf
+from pytorch_lightning.trainer import Trainer
+from pytorch_lightning import seed_everything
+import os
+seed_everything(1)
+
+@hydra.main(config_path="conf", config_name="config")
+def main(cfg: MainConfig):
+    print(OmegaConf.to_yaml(cfg))
+
+    task = hydra.utils.instantiate(cfg.task, _recursive_=False)
+
+    assert cfg.task.model.model_path == cfg.task.transform.model_path
+    transform = hydra.utils.instantiate(cfg.task.transform)
+    datamodule = hydra.utils.instantiate(cfg.datamodule, transform=transform)
+    checkpoint_callback = hydra.utils.instantiate(cfg.checkpoint_callback)
+    trainer = Trainer(**cfg.trainer, callbacks=[checkpoint_callback])
+
+    ckpt_path = cfg.task.load_from_checkpoint
+    trainer.test(
+        model=task,
+        ckpt_path=ckpt_path,
+        verbose=True,
+        datamodule=datamodule,
+    )
+
+if __name__ == "__main__":
+    main()
