d231db36c041fefe686c1b7d0eba6d1bcebd984d
Louis MARTIN <louismartincs@gmail.com>
2023-02-15 16:03:31 +0000

    Fix beginning whitespace bug and refactor SPMTransform.forward

--- bela/transforms/spm_transform.py
+++ bela/transforms/spm_transform.py
@@ -1,11 +1,28 @@
 #!/usr/bin/env python3
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 
-from typing import Optional
+from typing import List, Tuple, Optional
 import os
 import torch.nn as nn
 import sentencepiece as spm
-from .sentencepiece_pb2 import SentencePieceText
+from bela.transforms.sentencepiece_pb2 import SentencePieceText
+
+
+def convert_text_to_spm_tokens_with_char_offsets(text: str, spm_processor: spm.SentencePieceProcessor) -> List[Tuple[int, int, int]]:
+    spt = SentencePieceText()
+    spt.ParseFromString(spm_processor.encode_as_serialized_proto(text))
+    # Set offset to number of starting whitespaces (spm removes them)
+    start_offset = len(text) - len(text.lstrip())
+    token_ids_with_offsets = []
+    for piece in spt.pieces:
+        # NOTE: It seems we can't use piece.begin and piece.end because it sometimes break, e.g. with
+        # `text = "   Martina Steuk (née Kämpfert; born 11 November 1959) is a German former track and field athlete "
+        # At some point piece.begin will not correspond to the actual offset in the text
+        end_offset = start_offset + len(piece.surface)
+        assert text[start_offset:end_offset] == piece.surface, f"{text[start_offset:end_offset]} != {piece.surface}"
+        token_ids_with_offsets.append((piece.id, start_offset, end_offset))
+        start_offset = end_offset
+    return token_ids_with_offsets
 
 
 class SPMTransform(nn.Module):
@@ -24,27 +41,19 @@ class SPMTransform(nn.Module):
         self.add_special_tokens = add_special_tokens
 
     def forward(self, texts):
-        """Converts a list of strings to a list of lists of token ids along with their character offset (i.e. where they start in the string)."""
         output = []
         for text in texts:
-            spt = SentencePieceText()
-            spt.ParseFromString(self.processor.encode_as_serialized_proto(text))
-            token_ids_with_offsets = []
+            token_ids_with_offsets = convert_text_to_spm_tokens_with_char_offsets(text, self.processor)
+            # Add 1 to all token ids except for the unk token
+            token_ids_with_offsets = [
+                (token_id + 1 if token_id != 0 else self.unk_token_id, start_offset, end_offset)
+                for token_id, start_offset, end_offset in token_ids_with_offsets
+            ]
+            # Limit the number of tokens to max_seq_len (accounting for special tokens)
+            token_ids_with_offsets = token_ids_with_offsets[:self.max_seq_len - 2]
             if self.add_special_tokens:
-                token_ids_with_offsets.append((0,0,0))
-            for idx, piece in enumerate(spt.pieces):
-                if piece.id != 0:
-                    token_id = piece.id + 1
-                else:
-                    token_id = self.unk_token_id
-                token_ids_with_offsets.append((token_id, piece.begin, piece.end))
-
-                # take into account special tokens
-                if idx == self.max_seq_len - 3:
-                    break
-
-            if self.add_special_tokens:
-                token_ids_with_offsets.append((2, piece.end, 0))
-
+                last_offset = token_ids_with_offsets[-1][2]
+                token_ids_with_offsets = [(0, 0, 0)] + token_ids_with_offsets + [(2, last_offset, 0)]
+            assert len(token_ids_with_offsets) <= self.max_seq_len
             output.append(token_ids_with_offsets)
         return output
