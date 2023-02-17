2d8476c52f311bdb0a9bafdfd9deb75c632b24ff
Louis MARTIN <louismartincs@gmail.com>
2023-02-17 15:52:56 +0000

    Add script to do a grid search on thresholds

---
+++ scripts/grid_search_thresholds.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+from itertools import product
+
+from tqdm import tqdm
+import numpy as np
+
+from bela.evaluation.model_eval import ModelEval, load_file
+from bela.utils.prediction_utils import get_predictions_using_windows
+
+
+if __name__ == "__main__":
+    # Finetuned on aida
+    checkpoint_path = "/checkpoints/movb/bela/aida/lightning_logs/version_0/checkpoints/checkpoint_1.ckpt"
+    model_eval = ModelEval(checkpoint_path, config_name="joint_el_mel_new_index")
+
+    results = {}
+    print(f"{model_eval.checkpoint_path=}")
+    for md_threshold, el_threshold in tqdm(list(product(np.arange(0, 0.6, 0.2), repeat=2))):
+        model_eval.task.md_threshold = md_threshold
+        model_eval.task.el_threshold = el_threshold
+        print(f"{model_eval.task.md_threshold=}")
+        print(f"{model_eval.task.el_threshold=}")
+        test_data_path = "/fsx/louismartin/bela/retrieved_from_aws_backup/ndecao/TACKBP2015/train_bela_format_all_languages.jsonl"
+        print(f"Processing {test_data_path}")
+        test_data = load_file(test_data_path)
+        predictions = get_predictions_using_windows(model_eval, test_data, window_length=256)
+        (f1, precision, recall), (f1_boe, precision_boe, recall_boe) = ModelEval.compute_scores(test_data, predictions)
+        print(f"F1 = {f1:.4f}, precision = {precision:.4f}, recall = {recall:.4f}")
+        print(f"F1 boe = {f1_boe:.4f}, precision = {precision_boe:.4f}, recall = {recall_boe:.4f}")
+        results[(md_threshold, el_threshold)] = (f1, precision, recall), (f1_boe, precision_boe, recall_boe)
+    print(sorted(results.items(), key=lambda x: x[1][0][0], reverse=True))
+    pickle_path = Path.home() / "tmp/grid_search_thresholds.pkl"
