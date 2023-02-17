8f1c87bc53ad440918e4f5ead70750adbc2a241f
Louis MARTIN <louismartincs@gmail.com>
2023-02-17 20:12:44 +0000

    Update grid search

--- scripts/grid_search_thresholds.py
+++ scripts/grid_search_thresholds.py
@@ -9,8 +9,8 @@ from bela.utils.prediction_utils import get_predictions_using_windows
 
 
 if __name__ == "__main__":
-    # Model finetuned on TACKBP
-    checkpoint_path = "/data/home/louismartin/dev/BELA/multirun/2023-02-17/00-08-45/0/lightning_logs/version_0/checkpoints/checkpoint_best-v2.ckpt"
+    # Finetuned on aida
+    checkpoint_path = "/checkpoints/movb/bela/aida/lightning_logs/version_0/checkpoints/checkpoint_1.ckpt"
     model_eval = ModelEval(checkpoint_path, config_name="joint_el_mel_new_index")
 
     results = {}
@@ -20,10 +20,10 @@ if __name__ == "__main__":
         model_eval.task.el_threshold = el_threshold
         print(f"{model_eval.task.md_threshold=}")
         print(f"{model_eval.task.el_threshold=}")
-        test_data_path = "/fsx/louismartin/bela/retrieved_from_aws_backup/ndecao/TACKBP2015/train_bela_format_all_languages.jsonl",
+        test_data_path = "/fsx/louismartin/bela/retrieved_from_aws_backup/ndecao/TACKBP2015/train_bela_format_all_languages.jsonl"
         print(f"Processing {test_data_path}")
         test_data = load_file(test_data_path)
-        predictions = get_predictions_using_windows(model_eval, test_data, window_length=1024)
+        predictions = get_predictions_using_windows(model_eval, test_data, window_length=256)
         (f1, precision, recall), (f1_boe, precision_boe, recall_boe) = ModelEval.compute_scores(test_data, predictions)
         print(f"F1 = {f1:.4f}, precision = {precision:.4f}, recall = {recall:.4f}")
         print(f"F1 boe = {f1_boe:.4f}, precision = {precision_boe:.4f}, recall = {recall_boe:.4f}")
